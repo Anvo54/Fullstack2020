f613735c679c53e8cda9854d36854638c13d6064
Antti Vornanen <anttivornanen@Antti-iMac.home>
2020-04-07 21:08:58 +0300

    puhelinluettelo 2.16

--- osa2/puhelinluettelo/src/Components/App.js
+++ osa2/puhelinluettelo/src/Components/App.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react'
-import axios from 'axios'
+import personService from '../services/personService'
 
 const App = () => {
   const [ persons, setPersons] = useState([]) 
@@ -8,7 +8,7 @@ const App = () => {
   const [ filterName, setFilterName ] = useState('') 
 
 useEffect(()=> {
-	axios.get('http://localhost:3001/persons').then(response => {setPersons(response.data)})
+	personService.getAll().then(initialPersons => {setPersons(initialPersons)})
 }, [])
 
   const addName = (event) =>{
@@ -22,7 +22,9 @@ useEffect(()=> {
         name: newName,
         number: newNumber
       }
-      setPersons(persons.concat(newPerson))
+      personService.create(newPerson).then(returnedPerson => {
+        setPersons(persons.concat(returnedPerson))
+      })
     } else {
         alert(`${newName} is already added to phonebook`)
     }
