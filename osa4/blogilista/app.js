72b43d497497dc3e73154b9aad7668c58a56b122
Anvo54 <antti.vornanen@gmail.com>
2020-04-21 16:29:46 +0300

    Add Blogilista 4.2

---
+++ osa4/blogilista/app.js
@@ -0,0 +1,29 @@
+const config = require('./utils/config') // Täällä määritellään ympäristö muuttujat
+const express = require('express') // Tässä otetaan käyttöön express
+const app = express()
+const cors = require('cors')
+const blogsRouter = require('./controllers/blogs')
+const middleWare = require('./utils/middleware')
+const logger = require('./utils/logger')
+const mongoose = require('mongoose')
+
+logger.info('connecting to', config.MONGODB_URI)
+
+mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    logger.info('connected to MongoDB')
+  })
+  .catch(() => {
+    logger.error('error connection to MongoDB:', error.message)
+  })
+
+app.use(cors())
+app.use(express.json())
+app.use(middleWare.requestLogger)
+
+app.use('/api/blogs/', blogsRouter)
+
+app.use(middleWare.unknownEndpoint)
+app.use(middleWare.errorHandler)
+
+module.exports = app
