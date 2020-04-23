a35ad5420e15a4a17d8b1f2d3a0f4c40f35ffe7f
Anvo54 <antti.vornanen@gmail.com>
2020-04-23 20:18:05 +0300

    blogilista 4.5*: apufunktioita ja yksikkötestejä, step3

--- osa4/blogilista/utils/list_helper.js
+++ osa4/blogilista/utils/list_helper.js
@@ -3,11 +3,24 @@ const dummy = (blogs) => {
 }
 
 const totalLikes = (blogs) => {
-  const result = blogs.reduce(( ac, cv) => ac + cv.likes, 0)
+  const result = blogs.reduce(( ac, cv ) => ac + cv.likes, 0)
   return result
 }
 
+const favouriteBlog = (blogs) => {
+  const likes = blogs.map(b => b.likes)
+  const obj = blogs.find(blog => blog.likes === Math.max(...likes))
+  const result = {
+    'title': obj.title,
+    'author': obj.author,
+    'likes': obj.likes
+  }
+  return(result)
+}
+
+
 module.exports = {
   dummy,
-  totalLikes
+  totalLikes,
+  favouriteBlog
 }
