6c39bfd92bdf6d86f0a30928e90fbfa8fae6db6b
Anvo54 <antti.vornanen@gmail.com>
2020-05-01 12:29:02 +0300

    4.19: blogilistan laajennus, step8

--- osa4/blogilista/controllers/blogs.js
+++ osa4/blogilista/controllers/blogs.js
@@ -1,4 +1,5 @@
 const blogsRouter = require('express').Router()
+const jwt = require('jsonwebtoken')
 const Blog = require('../models/blog')
 const User = require('../models/user')
 
@@ -7,13 +8,25 @@ blogsRouter.get('/', async (request, response) => {
   response.json(blogs.map(blog => blog.toJSON()))
 })
 
+const getTokenFrom = request => {
+  const authorization = request.get('authorization')
+  if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
+    return authorization.substring(7)
+  }
+  return null
+}
+
 blogsRouter.post('/', async (request, response) => {
   const body = request.body
+  const token = getTokenFrom(request)
 
-  const usId = await User.find({})
-  const ids = usId.map(u => u.id)
-  // const user = await User.findById(body.userId)
-  const user = await User.findById(ids[0])
+  const decodedToken = jwt.verify(token, process.env.SECRET)
+  if (!token || !decodedToken.id) {
+    return response.status(401).json({
+      error: 'token missing or invalid'
+    })
+  }
+  const user = await User.findById(decodedToken.id)
   let likes = (!body.likes) ? 0 : body.likes
 
   const blog = new Blog({
