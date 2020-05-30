ce0005a05fff4d5c0b429acb874984bd1ab5ad98
Anvo54 <antti.vornanen@gmail.com>
2020-05-30 22:12:33 +0300

    7.18: kommentit, step2

--- osa7/bloglist-frontend/src/components/SingleBlog.js
+++ osa7/bloglist-frontend/src/components/SingleBlog.js
@@ -1,15 +1,24 @@
 import React, {useEffect} from 'react'
 import { likeBlog } from '../reducers/blogreducer'
 import { useDispatch, useSelector } from 'react-redux'
-import { initComments } from '../reducers/commentsReducer'
+import { initComments, postComment } from '../reducers/commentsReducer'
 
 const SingleBlog = ({blog, useParams}) => {
-  console.log(blog)
   const comments = useSelector(state => state.comments)
   const dispatch = useDispatch()
   const id = useParams().id
   const sBlog = blog.find(b => b.id === id)
   const blogsComments = comments.filter(c => c.blog === id)
+  
+  const addComment =(event)=> {
+    event.preventDefault()
+    const commentObject = {
+      comment: event.target.newComment.value,
+      blog: id
+    }
+    dispatch(postComment(commentObject))
+    event.target.newComment.value = ''
+  }
   
   useEffect(() => {
     dispatch(initComments(id))
@@ -33,6 +42,11 @@ const SingleBlog = ({blog, useParams}) => {
       <div>added by {sBlog.author}</div>
       <br></br>
       <h3>comments</h3>
+      <form onSubmit={addComment}>
+        <input name='newComment'></input>
+        <button type='submit'>Add comment</button>
+      </form>
+      <br></br>
       {blogsComments.map(c => <li key={c.id}>{c.comment}</li>)}
     </div>
   )
--- osa7/bloglist-frontend/src/reducers/commentsReducer.js
+++ osa7/bloglist-frontend/src/reducers/commentsReducer.js
@@ -2,10 +2,21 @@ import commentService from '../services/comments'
 
 export const initComments = () => {
   return async dispatch => {
-    const comments = await commentService.getBlog()
+    const comments = await commentService.getComment()
     dispatch({
       type: 'GET_COMMENTS',
       data: comments
+    })
+  }
+}
+
+export const postComment = (content) => {
+  return async dispatch => {
+    const comment = await commentService.postComment(content)
+    console.log(comment)
+    dispatch({
+      type: 'NEW_COMMENT',
+      data: comment
     })
   }
 }
@@ -14,6 +25,8 @@ const commentReducer = (state = [], action) =>{
   switch (action.type) {
     case 'GET_COMMENTS':
       return action.data
+    case 'NEW_COMMENT':
+      return [...state, action.data]
     default:
       return state
   }
